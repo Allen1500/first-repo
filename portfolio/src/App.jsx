2119c3aa08fb892704e6a1bd80b68707f2685248
Allen1500 <allenbook96@gmail.com>
2025-02-12 09:23:09 -0600

    git commit -m "I started to set up nav in the components folder in the Navbar file"
    git add .
    git commit -m "I started to set up nav in the components folder in the Navbar file"
    git push origin main

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg'//
 import './App.css'
 
 function App() {
+  return <div className='App'>NavBar</div>
   const [count, setCount] = useState(0)
 
   return (
